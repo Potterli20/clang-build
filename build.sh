704e4d50b0fa21c333ef33d3b28ce9d5630e8cf6
trli <potter@trli.club>
2023-03-05 12:31:50 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 ./build-llvm.py -D \
-LLVM_PARALLEL_COMPILE_JOBS=6 \
-LLVM_PARALLEL_LINK_JOBS=4 \
+LLVM_PARALLEL_COMPILE_JOBS=$(nproc) \
+LLVM_PARALLEL_LINK_JOBS=$(nproc) \
 --lto thin
 
 ./build-binutils.py -t aarch64-linux-gnu
